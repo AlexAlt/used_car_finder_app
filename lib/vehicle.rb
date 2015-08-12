6f9b5ee28b39f701b23e4ce5ba83b3c1201a5caf
Alex Altieri & Alyssa Horrocks <student@epicodus.com>
2015-08-12 09:40:29 -0700

    Complete the setup of the Vehicle class to allow a vehicle's make, model, and year to be saved in the Vehicle list

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,5 +1,8 @@
 class Vehicle
   define_method(:initialize) do |make, model, year|
+    @@all_vehicles = []
+
+    @id = @@all_vehicles.length().+(1)
     @make = make
     @model = model
     @year = year
@@ -15,5 +18,21 @@ class Vehicle
 
   define_method(:year) do
     @year
+  end
+
+  define_singleton_method(:all) do
+    @@all_vehicles
+  end
+
+  define_method(:save) do
+    @@all_vehicles.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@all_vehicles = []
+  end
+
+  define_method(:id) do
+    @id
   end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -2,6 +2,10 @@ require('rspec')
 require('vehicle')
 
 describe('Vehicle') do
+  before() do
+    Vehicle.clear()
+  end
+
   describe('#make') do
     it('returns the make of the car') do
       test_vehicle = Vehicle.new("Toyota", "Prius", 2015)
@@ -22,4 +26,34 @@ describe('Vehicle') do
       expect(test_vehicle.year()).to(eq(2015))
     end
   end
+
+  describe('.all') do
+    it('begins as a blank array') do
+      expect(Vehicle.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves a vehicle to list of vehicles') do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2015)
+      test_vehicle.save()
+      expect(Vehicle.all()).to(eq([test_vehicle]))
+    end
+  end
+
+  describe('.clear') do
+    it('clear the list of vehicles') do
+      Vehicle.clear()
+      expect(Vehicle.all()).to(eq([]))
+    end
+  end
+
+  describe('#id') do
+    it('returns the id of the vehicle') do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2015)
+      test_vehicle.save()
+      expect(test_vehicle.id()).to(eq(1))
+    end
+  end
+
 end
